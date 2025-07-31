a3439d718d54680c720d9d67f4ffe8fbc650e419
NMBY21 <betelihemmengist21@gmail.com>
2025-07-31 22:10:04 +0300

    basic SQL queries

---
+++ wk-1-NMBY21/answers.sql
@@ -0,0 +1,7 @@
+-- Database Assignment One
+
+-- 1. This command will create a new database called salesDB.
+CREATE DATABASE salesDB;
+
+-- 2. This command will drop (delete) the database called demo.
+DROP DATABASE demo;
